1266ac849dbfc7df6777bb050e073e52386e4a32
yun <huayun321@gmail.com>
2022-04-26 15:09:21 +0800

    repl with editline

--- ch2/hello_world.c
+++ ch2/hello_world.c
@@ -4,3 +4,5 @@ int main(int argc, char** argv) {
     puts("Hello, world!");
     return 0;
 }
+
+//cc -std=c99 -Wall hello_world.c -o hello_world
--- ch4/repl.c
+++ ch4/repl.c
@@ -15,3 +15,5 @@ int main(int argc, char** argv) {
     
     return 0;
 }
+
+//cc -std=c99 -Wall repl.c -o repl  
