449237991851ae36fe812ad8a84c12a1fe86c1ff
yun <huayun321@gmail.com>
2022-04-26 15:22:18 +0800

    fix name

--- ch4/repl.c
+++ ch4/repl.c
@@ -3,12 +3,12 @@
 static char input[2048];
 
 int main(int argc, char** argv) {
-    puts("Lispy Version 0.0.0.0.1");
+    puts("Yun Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
     while (1)
     {
-        fputs("Lispy> ", stdout);
+        fputs("Yun> ", stdout);
         fgets(input, 2048, stdin);
         printf("No you're a %s", input);
     }
